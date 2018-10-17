f598d3a5525423e0571868dbdd57645c0f7021c4
pengqh <pengquanhua@yeah.net>
2018-10-17 15:07:21 +0800

    add QHNodeViewController

---
+++ v2exnew/Controllers/Contents/QHNodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  QHNodeViewController.h
+//  v2exnew
+//
+//  Created by pengquanhua on 2018/10/17.
+//  Copyright © 2018年 pengquanhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QHNodeViewController : UIViewController
+
+@end
---
+++ v2exnew/Controllers/Contents/QHNodeViewController.m
@@ -0,0 +1,37 @@
+//
+//  QHNodeViewController.m
+//  v2exnew
+//
+//  Created by pengquanhua on 2018/10/17.
+//  Copyright © 2018年 pengquanhua. All rights reserved.
+//
+
+#import "QHNodeViewController.h"
+
+@interface QHNodeViewController ()
+
+@end
+
+@implementation QHNodeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
